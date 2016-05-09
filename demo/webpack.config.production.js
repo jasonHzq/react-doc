d7a051e8c6176d23612a1d4a67279d50e9fdfa0a
jasonHzq <hzongquan@126.com>
2016-05-09 22:15:17 +0800

    deploy 1.0.2

--- demo/webpack.config.production.js
+++ demo/webpack.config.production.js
@@ -14,7 +14,7 @@ module.exports = {
   },
   resolve: {
     alias: {
-      'react-doc': path.join(__dirname, '..', 'src/index.js'),
+      'react-doc': path.join(__dirname, '..', 'lib/index.js'),
     },
   },
   webpackServer: {
@@ -27,8 +27,6 @@ module.exports = {
         loader: 'babel-loader',
         include: [
           __dirname,
-          path.join(__dirname, '..', 'src'),
-          path.join(__dirname, '..', 'node_modules'),
         ],
         exclude: [
           path.join(__dirname, '..', 'node_modules/babel-standalone'),
